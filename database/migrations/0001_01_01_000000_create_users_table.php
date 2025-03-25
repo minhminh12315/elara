d3658fe58c923f101cd59ebaf5d50599b4e4af12
Lê Đức Ân <162079456+anduc0104@users.noreply.github.com>
2025-03-25 23:24:23 +0700

    An: fix model user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('name');
             $table->string('PhoneNumber', 15)->nullable();
             $table->string('ID_Passport', 20)->nullable();
             $table->string('email')->unique();
